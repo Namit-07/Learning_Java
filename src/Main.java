c0638fb61549d025a78a21d8a3bf5ee3cfdaeb87
Namit-07 <namittickoo2006@gmail.com>
2025-05-22 13:16:10 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+     static public void main(String[] args) {
+        System.out.println("Sum of 3 different numbers");
+        int num1= 9;
+        int num2= 27;
+        int num3= 86;
+        int sum= num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
+// AddTwoNumbers --> PascalConvention (for classes)
+// addTwoNumbers --> camelCaseConvention (for functions)
