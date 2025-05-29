5fa9d1d2e2d0b97e0d5a01a8cc8a43974fb3f1ea
Namit-07 <namittickoo2006@gmail.com>
2025-05-29 22:30:19 +0530

    init commit

---
+++ src/CWH_26_arrays.java
@@ -0,0 +1,29 @@
+public class CWH_26_arrays {
+    public static void main(String[] args) {
+        /* Classroom of 500 students - You have to store the marks of 500 students.
+        You have 2 options:
+        1. Create 500 variables.
+        2. Use arrays (recommended).
+         */
+        // There are three main ways to create an array in java
+        // 1. Declaration and memory allocation
+        // int [] marks = new int[5];
+
+        // 2. Declaration and then memory allocation
+        // int [] marks;
+        // marks = new int[5];
+        // Intialization
+        //marks[0] = 100;
+        //marks[1] = 60;
+        //marks[2] = 70;
+        //marks[3] = 90;
+        //marks[4] = 86;
+
+        // 3. Declaration, memory allocation amd initialization together
+
+
+        //marks[5] = 96; - throws an error
+        System.out.println(marks[4]);
+
+    }
+}
