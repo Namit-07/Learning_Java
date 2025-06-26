df730bc1a20ae3ccc4c7ff0314b1e7cf3c61c748
Namit-07 <namittickoo2006@gmail.com>
2025-06-26 13:51:47 +0530

    thread methods, some practice and errors

--- src/Practice Sets/cwh_PracSet_ch13.java
+++ src/Practice Sets/cwh_PracSet_ch13.java
@@ -1,2 +1,44 @@
+class Greet extends Thread{
+    public void run(){
+        while(true){
+            System.out.println("Good morning");
+        }
+    }
+}
+class Welcome extends Thread{
+    public void run(){
+        while (true){
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("Welcome");
+        }
+    }
+}
+
 public class cwh_PracSet_ch13 {
+    public static void main(String[] args) {
+        // Question 1.
+         Greet g1 = new Greet();
+         Welcome w1 = new Welcome();
+         // g1.start();
+         // w1.start();
+
+        // Question 2.
+        // add a delay of 200ms
+
+        // Question 3.
+        g1.setPriority(6);
+        w1.setPriority(9);
+        System.out.println(g1.getPriority());
+        System.out.println(w1.getPriority());
+
+        // Question 4
+        System.out.println(w1.getState());
+        System.out.println(g1.getState());
+        System.out.println(Thread.currentThread().getState());
+
+    }
 }
